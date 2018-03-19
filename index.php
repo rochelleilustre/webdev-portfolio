e9a715a322b8a54f91fade0f72e619c5479237e2
rochelle ilustre <chellie.ilustre@gmail.com>
2018-03-19 17:48:50 +0800

    webdev 2

---
+++ index.php
@@ -0,0 +1,352 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-compatible" content="IE=edge">
+    
+
+    <title>Rochelle Benet Ilustre | Online Portfolio</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/stylish-portfolio.css">
+    <link href="css/stylish-portfolio.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+    <!-- Navigation -->
+    <a class="menu-toggle rounded" href="#">
+      <i class="fa fa-bars"></i>
+    </a>
+    <nav id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+          <a class="js-scroll-trigger" href="#page-top"><b><i class="fa fa-signal" aria-hidden="true"></i> RBI</b></a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#page-top">Home</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#about">About</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#services">Skills</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#portfolio">Portfolio</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead d-flex">
+      <div class="container text-center my-auto">
+        <img id="me" src="img/profile.jpg" width="200">
+        <h1 class="mb-1">Hello! I'm Chellie.</h1>
+
+        <h4 class="mb-5">
+          <em>Aspiring Web Developer + Graphic Artist</em>
+        </h4>
+        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#portfolio">PORTFOLIO</a>
+      </div>
+      <div class="overlay"></div>
+    </header>
+
+    <!-- About -->
+    <section class="content-section bg-light" id="about">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h2>[ DESIGN + CODE + SLEEP + REPEAT ]</h2>
+            <p class="lead mb-5"><i>"Do it with PASSION or not at all.."</i></p>
+            <!-- <a class="btn btn-dark btn-xl js-scroll-trigger" href="#services">What We Offer</a> -->
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Skills -->
+    <section class="content-section bg-primary text-white text-center" id="services">
+      <div class="container">
+        
+        <div class="content-section-heading">
+          <h3 class="text-secondary mb-0">SKILLS + LANGUAGES</h3>
+          <br><br>
+        </div><!-- content-section-heading -->
+
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3"> -->
+              <img src="img/html.png" width="120">
+            <!-- </span> -->
+            <h4><strong>HTML5</strong></h4>
+            <!-- <p class="text-faded mb-0">Looks great on any screen size!</p> -->
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3"> -->
+              <img src="img/css.png" width="90">
+            <!-- </span> -->
+            <h4><strong>CSS3</strong></h4>
+            <!-- <p class="text-faded mb-0">Freshly redesigned for Bootstrap 4.</p> -->
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3"> -->
+              <img src="img/js.png" width="80">
+            <!-- </span> -->
+            <h4><strong>JAVASCRIPT</strong></h4>
+            <!-- <p class="text-faded mb-0">Millions of users
+             <i class="fa fa-heart"></i>
+              Start Bootstrap!</p> -->
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-mustache"></i>-->
+            <img src="img/bootstrap.png" width="100">
+              <!-- </span> -->
+            <br><br>
+            <h4><strong>BOOTSTRAP</strong></h4>
+
+            <!-- <p class="text-faded mb-0">I mustache you a question...</p> -->
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+      <br><br>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-screen-smartphone"></i>
+            </span> -->
+            <img src="img/jquery.png" width="150">
+            <br><br>
+            <h4><strong>JQUERY</strong></h4>
+            <!-- <p class="text-faded mb-0">Looks great on any screen size!</p> -->
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-pencil"></i>
+            </span> -->
+            <img src="img/php.png" width="150">
+            <br><br><br>
+            <h4><strong>PHP</strong></h4>
+            <!-- <p class="text-faded mb-0">Freshly redesigned for Bootstrap 4.</p> -->
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-like"></i>
+            </span> -->
+            <img src="img/sql.png" width="120">
+            <br>
+            <h4><strong>MY SQL</strong></h4>
+            <!-- <p class="text-faded mb-0">Millions of users
+              <i class="fa fa-heart"></i>
+              Start Bootstrap!</p> -->
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-mustache"></i>
+            </span> -->
+            <img src="img/ps.png" width="120">
+            <h4><strong>PHOTOSHOP</strong></h4>
+            <!-- <p class="text-faded mb-0">I mustache you a question...</p> -->
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+      <br><br>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-screen-smartphone"></i>
+            </span> -->
+            <img src="img/ai.png" width="150">
+            <h4><strong>ILLUSTRATOR</strong></h4>
+            <!-- <p class="text-faded mb-0">Looks great on any screen size!</p> -->
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-pencil"></i>
+            </span> -->
+            <img src="img/in.png" width="150">
+            <br><br><br>
+            <h4><strong>INDESIGN</strong></h4>
+            <!-- <p class="text-faded mb-0">Freshly redesigned for Bootstrap 4.</p> -->
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <!-- <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-like"></i>
+            </span> -->
+            <img src="img/lr.png" width="150">
+            <h4><strong>LIGHTROOM</strong></h4>
+            <!-- <p class="text-faded mb-0">Millions of users
+              <i class="fa fa-heart"></i>
+              Start Bootstrap!</p> -->
+          </div>
+          <!-- <div class="col-lg-3 col-md-6">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-mustache"></i>
+            </span>
+            <h4>
+              <strong>Photoshop</strong>
+            </h4>
+            <p class="text-faded mb-0">I mustache you a question...</p>
+          </div> -->
+        </div>
+      </div>
+    </section>
+
+    <!-- Callout -->
+    <section class="callout">
+      <div class="container text-center">
+        <h2 class="mx-auto mb-5">ABOUT
+          <em>me</em>
+          COMING SOON!</h2>
+        <a class="btn btn-primary btn-xl" href="#">Download My CURRICULUM VITAE!</a>
+      </div>
+    </section>
+
+    <!-- Portfolio -->
+    <section class="content-section" id="portfolio">
+      <div class="container">
+        <div class="content-section-heading text-center">
+          <h3 class="text-secondary mb-0">Portfolio</h3>
+          <h2 class="mb-5">Recent Projects</h2>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="https://rochelleilustre.github.io/b3nc-ilustre-rochelle/csp1/index.html">
+              <span class="caption">
+                <span class="caption-content">
+                  <!-- <h2>MAXIMUS PRIME</h2> -->
+                  <!-- <p class="mb-0">A static webpage</p> -->
+                </span>
+              </span>
+              <img class="img-fluid" src="img/maximus_prime-1.jpg" height="370" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <!-- <h2>SCENT OF ADA</h2> -->
+                  <!-- <p class="mb-0">An E-commerce website that caters different perfumes!</p> -->
+                </span>
+              </span>
+              <img class="img-fluid" src="img/csp2.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <!-- <h2>Strawberries</h2> -->
+                  <!-- <p class="mb-0">Strawberries are such a tasty snack, especially with a little sugar on top!</p> -->
+                </span>
+              </span>
+              <img class="img-fluid" src="img/Capture.JPG" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <!-- <h2>Workspace</h2> -->
+                  <!-- <p class="mb-0">A yellow workspace with some scissors, pencils, and other objects.</p> -->
+                </span>
+              </span>
+              <img class="img-fluid" src="img/MattePaintProject_rev2.jpg" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Call to Action -->
+    <section class="content-section bg-primary text-white">
+      <div class="container text-center">
+        <h2 class="mb-4">Get in touch!</h2>
+        <p>Got a job for me? <br>
+        Feel free to leave me a message and <br> I'll get back with you the soonest.</p>
+        <a href="#" class="btn btn-xl btn-light mr-4">Click Me!</a>
+        <a href="#" class="btn btn-xl btn-dark">Look at Me!</a>
+      </div>
+    </section>
+
+    <!-- Map -->
+    <!-- <section id="contact" class="map">
+      <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+      <br/>
+      <small>
+        <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a>
+      </small>
+    </section> -->
+
+    <!-- Footer -->
+    <footer class="footer text-center">
+      <div class="container">
+        <ul class="list-inline mb-5">
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white mr-3" href="#">
+              <i class="icon-social-linkedin"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white mr-3" href="#">
+              <i class="icon-social-facebook"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white" href="#">
+              <i class="icon-social-github"></i>
+            </a>
+          </li>
+        </ul>
+        <p class="text-muted small mb-0">Copyright &copy; Rochelle Benet Ilustre 2018</p>
+      </div>
+    </footer>
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+      <i class="fa fa-angle-up"></i>
+    </a>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/stylish-portfolio.min.js"></script>
+
+  </body>
+
+</html>
